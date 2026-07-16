256708fa1823e593dd64fa0b7105aa1a5199a523
taufique1524 <taufiquekhan786.edu@gmail.com>
2026-07-16 17:41:01 +0530

    Time: 7 ms (91.66%) | Memory: 22.4 MB (48.71%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,26 +1,57 @@
-class Solution {
+class DSU{
+private:
+    vector<int> parent , rank;
 public:
-    void dfs(int i , vector<int> & vis , vector<vector<int>> & stones){
-        vis[i] = 1;
-        for(int j=0;j<stones.size();j++){
-            if(vis[j]==0){
-                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
-                    dfs(j,vis,stones);
-                }
-            }
+    DSU(int n){
+        parent.resize(n);
+        rank.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+            rank[i] = 0;
         }
     }
+    int findParent(int node){
+        if(parent[node]==node) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    void un(int u , int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if(pu==pv) return;
+        if(rank[pu]<rank[pv]){
+            parent[pu] = pv;
+        }else if(rank[pv]<rank[pu]){
+            parent[pv] = pu;
+        }else{
+            parent[pu] = pv;
+            rank[pv]++;
+        }
+    }
+};
+class Solution {
+public:
     int removeStones(vector<vector<int>>& stones) {
-        int cnt = 0;
+        int mr = -1 , mc = -1;
         int n = stones.size();
-        vector<int> vis(n,0);
-        int grp = 0;
-        for(int i=0 ; i<n;i++){
-            if(vis[i]==0){
-                grp++;
-                dfs(i,vis,stones);
+        for(int i=0;i<n;i++){
+            mr = max(mr , stones[i][0]);
+            mc = max(mc , stones[i][1]);
+        }
+        DSU dj(mr+mc+2);
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            int r = stones[i][0];
+            int c = stones[i][1]+mr+1;
+            dj.un(r,c);
+            mp[r] = 1;
+            mp[c] = 1;
+        }
+        int cnt =0;
+        for(auto & it : mp){
+            if(it.first==dj.findParent(it.first)){
+                cnt++;
             }
         }
-        return n-grp;
+        return n-cnt;
     }
 };
